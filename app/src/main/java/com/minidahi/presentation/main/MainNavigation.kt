5f49aa3427f2ae8a5ef7a378db6523a9d2855094
chelebyy <muhammett.celebi@gmail.com>
2025-01-27 23:28:01 +0300

    Navigation hatası düzeltildi. Yaş grupları için geçici ekranlar eklendi.

--- app/src/main/java/com/minidahi/presentation/main/MainNavigation.kt
+++ app/src/main/java/com/minidahi/presentation/main/MainNavigation.kt
@@ -1,11 +1,17 @@
 package com.minidahi.presentation.main
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.minidahi.presentation.screens.home.HomeScreen
 import com.minidahi.presentation.screens.splash.SplashScreen
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun MainNavigation() {
@@ -21,6 +27,31 @@ fun MainNavigation() {
         
         composable(Screen.Home.route) {
             HomeScreen(navController = navController)
+        }
+
+        composable(Screen.AgeGroup3To5.route) {
+            // Geçici olarak boş bir ekran
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text("3-5 Yaş Grubu")
+            }
+        }
+
+        composable(Screen.AgeGroup6To8.route) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text("6-8 Yaş Grubu")
+            }
+        }
+
+        composable(Screen.AgeGroup9To10.route) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text("9-10 Yaş Grubu")
+            }
+        }
+
+        composable(Screen.ParentControl.route) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text("Ebeveyn Kontrol Paneli")
+            }
         }
     }
 }
